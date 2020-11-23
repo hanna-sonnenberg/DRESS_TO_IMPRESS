ede0271e3225640bd6589e49a20484f2f79294c2
hanna sonnenberg <sonnenberghanna@googlemail.com>
2020-11-23 17:57:59 +0100

    started with html structure for home page

--- db/migrate/20201123160223_create_bookings.rb
+++ db/migrate/20201123160223_create_bookings.rb
@@ -3,7 +3,7 @@ class CreateBookings < ActiveRecord::Migration[6.0]
     create_table :bookings do |t|
       t.string :address
       t.references :outfit, null: false, foreign_key: true
-      t.references :ures, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.date :start_date
       t.date :end_date
 
