e733c41a87c76ab368d9404c7831f272abcbf9d3
Alena Gritsiuk <alena22292@mail.ru>
2020-11-23 17:10:39 +0100

    Add validations for outfit

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,4 +1,10 @@
 class Outfit < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
+
+  validates :name, presence: true
+  validates :size, presence: true, inclusion: { in: %w(xs s m l xl) }
+  validates :gender, presence: true, inclusion: { in: %w(male female) }
+  validates :price, presence: true
+  validates :category, presence: true, inclusion: { in: %w(gula ) }
 end
