bc057d80add4737570068105763ebecaa11d7544
hanna sonnenberg <sonnenberghanna@googlemail.com>
2020-11-24 12:13:15 +0100

    changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-new_one = User.create!(last_name: "Kap", first_name: "Kelly", password: 123456, email: "d@jkfhdj.com")
+Outfit.destroy_all
+User.destroy_all
+
+User.create!(last_name: "Kap", first_name: "Kelly", password: 123456, email: "dghj@jkhdj.com")
+User.create!(last_name: "Sonnenberg", first_name: "Hanna", password: 345677, email: "abl@hshjnn.com")
+
+SIZES = ["xs", "s", "m", "l", "xl"]
+
+User.all.each do |user|
+  3.times do
+    Outfit.create(
+    name: "Witch",
+    size: SIZES.sample,
+    gender: "female",
+    price: rand(10..50),
+    category: "halloween",
+    location: "amsterdam",
+    start_date: Date.new(2020,5,23),
+    end_date: Date.new(2020,5,24),
+    user: user
+    )
+  end
+end
+
+
+puts "#{Outfit.count} new outfits created"
+puts "#{User.count} new user created"
