c8188646b73a01aa36058045b90a5b13d28593cb
Alena Gritsiuk <alena22292@mail.ru>
2020-11-24 15:00:11 +0100

    Create a booking and review forms

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :outfit
+
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: {
+    in: 0..5
+  }
+  validates :content, presence: true, length: { minimum: 5 }
 end
