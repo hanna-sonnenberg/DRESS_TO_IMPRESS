5abf2bd874f4b2eaf31e156809d2f2ead13ad1c6
Alena Gritsiuk <alena22292@mail.ru>
2020-11-25 11:29:21 +0100

    Create a descripption field for outfit model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_151542) do
+ActiveRecord::Schema.define(version: 2020_11_25_102736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_151542) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["user_id"], name: "index_outfits_on_user_id"
   end
 
